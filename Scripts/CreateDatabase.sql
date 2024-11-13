2fb7edda9ff05cd527668cf5a269641a47bcb3d7
Cintya Miotto <cintyahx@gmail.com>
2024-11-13 06:20:18 -0300

    connection db fixed

--- Scripts/CreateDatabase.sql
+++
@@ -1,8 +0,0 @@
-USE [master];
-GO
-
-IF NOT EXISTS(SELECT name FROM master.sys.databases WHERE name = 'TasksDb')
-BEGIN
-	CREATE DATABASE TasksDb;
-END
-GO
--- run-dev-sqlserver.cmd
+++ run-dev-sqlserver.cmd
@@ -1,1 +1,1 @@
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD =2Secure*Password2" -p 5533:1433 --name TasksDB -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=2Secure*Password2" -p 5533:1433 --name TasksDB -d mcr.microsoft.com/mssql/server:2019-latest
